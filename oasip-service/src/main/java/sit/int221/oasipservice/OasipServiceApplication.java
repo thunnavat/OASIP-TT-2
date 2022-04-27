b21b40f887ba56df4a940201e1e7dfb1c50ff7a5
thunnavat <thunnavat.c@mail.kmutt.ac.th>
2022-04-27 15:57:54 +0700

    initial proj

---
+++ oasip-service/src/main/java/sit/int221/oasipservice/OasipServiceApplication.java
@@ -0,0 +1,13 @@
+package sit.int221.oasipservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OasipServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OasipServiceApplication.class, args);
+    }
+
+}
---
+++ oasip-service/src/test/java/sit/int221/oasipservice/OasipServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package sit.int221.oasipservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OasipServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
