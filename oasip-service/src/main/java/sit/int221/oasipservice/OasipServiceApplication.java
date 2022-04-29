d872fd260e9646d3270949138e0b467a1e70df72
thunnavat <thunnavat.c@mail.kmutt.ac.th>
2022-04-29 22:44:52 +0700

    Fixed Cors V2

--- oasip-service/src/main/java/sit/int221/oasipservice/OasipServiceApplication.java
+++ oasip-service/src/main/java/sit/int221/oasipservice/OasipServiceApplication.java
@@ -2,12 +2,25 @@ package sit.int221.oasipservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class OasipServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OasipServiceApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
     }
 
 }
